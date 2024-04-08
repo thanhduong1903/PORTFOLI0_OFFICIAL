45d9f830495162a4e5810aa2bfccaef7c952b0a7
Mathew Ly <145820158+thanhduong1903@users.noreply.github.com>
2024-04-08 21:04:40 +0700

    committed

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PORTFOLI0_OFFICIAL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿function toggleMenu(){
+ const menu = document.querySelector(".mini-link");
+ const icon = document.querySelector(".clsShowHide");
+ menu.classList.toggle("open");
+ icon.classList.toggle("open");
+}
